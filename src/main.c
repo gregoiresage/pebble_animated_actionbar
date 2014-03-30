1286cb943652b8fc60e889e2f69503d72cc4c593
Grégoire Sage <gregoire.sage@gmail.com>
2014-03-30 01:18:13 +0100

    remove warnings

--- src/main.c
+++ src/main.c
@@ -8,18 +8,14 @@ static GBitmap *action_icon_plus;
 static GBitmap *action_icon_minus;
 
 static void click_handler(ClickRecognizerRef recognizer, Window *window) {
-  Layer *layer = action_bar_layer_get_layer(action_bar);
-  
-  GRect to_rect = layer_get_bounds(layer);
-
   switch(click_recognizer_get_button_id(recognizer)) {
-	case BUTTON_ID_DOWN: 
-	  hide_actionbar(action_bar);
-    break;
-	default:
-  case BUTTON_ID_UP:
-	  show_actionbar(action_bar);
-    break;
+    case BUTTON_ID_DOWN: 
+      hide_actionbar(action_bar);
+      break;
+    default:
+    case BUTTON_ID_UP:
+      show_actionbar(action_bar);
+      break;
   }
 }
 
@@ -40,7 +36,7 @@ void handle_init(void) {
   action_bar_layer_add_to_window(action_bar, my_window);
   // Set the click config provider:
   action_bar_layer_set_click_config_provider(action_bar, click_config_provider);
-  
+
   // Set the icons:
   // The loading the icons is omitted for brevity... See HeapBitmap.
   action_bar_layer_set_icon(action_bar, BUTTON_ID_UP, action_icon_plus);
